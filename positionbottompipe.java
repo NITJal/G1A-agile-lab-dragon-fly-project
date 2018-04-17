38709170a19d5c8699e5c3f6c0206b967dc9458d
Amritpal9 <Amritpalsinghgahir@gmail.com>
2018-04-17 23:06:17 +0530

    Comments with enhancement

--- positionbottompipe.java
+++ positionbottompipe.java
@@ -5,7 +5,6 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 public class BottomPipe {
-	//global variables
 	private Image bottomPipe;
 	private int xLoc = 0, yLoc = 0;
 	
@@ -14,19 +13,24 @@ public class BottomPipe {
 		scaleBottomPipe(initialWidth, initialHeight);
 	}
 	
+	 //Scale to desired width n height the bottomPipe
 	public void scaleBottomPipe(int width, int height) {
 		bottomPipe = bottomPipe.getScaledInstance(width, height, Image.SCALE_SMOOTH);		
 	}
 	
+	//return Image
 	public Image getPipe() {
 		return bottomPipe;
 	}
 	
 	
+	 // return width
+	 
 	public int getWidth() {
 		return bottomPipe.getWidth(null);
 	}
 	
+// // return height
 	public int getHeight() {
 		return bottomPipe.getHeight(null);
 	}
@@ -39,19 +43,36 @@ public class BottomPipe {
 		xLoc = x;
 	}
 	
+	//return x locationof pipe
 	public int getX() {
 		return xLoc;
 	}
 	
-	
+	//get y location of pipe
 	public void setY(int y) {
 		yLoc = y;
 	}
 	
-
+	//return y location of pipe
 	public int getY() {
 		return yLoc;
 	}
 	
 	
+	 // return Rectangle outlining BottomPipe's position
+	 
+	public Rectangle getRectangle() {
+		return (new Rectangle(xLoc, yLoc, bottomPipe.getWidth(null), bottomPipe.getHeight(null)));
+	}
+	
+	
+	 //return pipe BufferedImage object
+	 
+	public BufferedImage getBI() {
+		BufferedImage bi = new BufferedImage(bottomPipe.getWidth(null), bottomPipe.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		Graphics g = bi.getGraphics();
+		g.drawImage(bottomPipe, 0, 0, null);
+		g.dispose();
+		return bi;
+	}
 }
